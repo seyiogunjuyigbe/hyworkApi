ff47359c0477931285d8095d00d9576dd2b7a2e2
Damola Adewunmi <damolasd@gmail.com>
2020-01-24 12:56:23 +0100

    added Schema definitions to User model

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-import {DB_URL}  from '../config/constants';
+import {LOCAL_DB_URL}  from '../config/constants';
 export const startDb = () =>{
-    mongoose.connect(DB_URL, {
+    mongoose.connect(LOCAL_DB_URL, {
         useNewUrlParser: true, 
         useFindAndModify: false, 
         useCreateIndex:true,
@@ -11,7 +11,7 @@ export const startDb = () =>{
           if(err){
             console.error(err);
           } else{
-            console.log('Database connected to: ' + DB_URL);
+            console.log('Database connected to: ' + LOCAL_DB_URL);
 
           }
       })
--- src/models/User.js
+++ src/models/User.js
@@ -1,157 +1,243 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
-const userSchema = new Schema({
-  username: {
-    type: String,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      unique: true
     },
-  firstName: {
-    type: String,
+    firstName: {
+      type: String,
+      trim: true
     },
-  lastName: {
-    type: String,
+    lastName: {
+      type: String,
+      trim: true
     },
-  phoneNumber: {
-    type: String,
+    phoneNumber: {
+      type: String,
+      trim: true
     },
-  phoneNumber2: {
-    type: String,
+    phoneNumber2: {
+      type: String,
+      trim: true
     },
-  email: {
-    type: String,
+    email: {
+      type: String,
+      trim: true,
+      unique: true
     },
-  email2: {
-    type: String,
+    email2: {
+      type: String,
+      trim: true
     },
-  dob: {
-    type: Date,
+    password: {
+      type: String,
+      required: true,
+      minlength: 8
     },
-  maritalStatus: {
-    type: String,
+    dob: {
+      type: Date
     },
-  category: {
-    type: String
-  },
-  employeeId: {
-    type: String,
+    maritalStatus: {
+      type: String
     },
-  role: {
-    type: String,
+    category: {
+      type: String
     },
-  joinDate: {
-    type: Date,
+    employeeId: {
+      type: String
     },
-  timeJoined: {
-    type: String,
+    role: {
+      type: String
     },
-  timezone: {
-    type: String,
+    joinDate: {
+      type: Date
     },
-  address: {
-    type: String,
+    timeJoined: {
+      type: String
     },
-  employmentStatus: {
-    type: String,
+    timezone: {
+      type: String
     },
-  tasks: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Task'
-  }],
-  files: [{
-    type: Schema.Types.ObjectId,
-    ref: 'File'
-  }],
-  assets: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Asset'
-  }],
-  dependents: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Dependent'
-  }],
-  travels: {
-    type: Schema.Types.ObjectId,
-    ref: 'Travel'
-  },
-  reportingTo: [{
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-    }],
-  skills:[],
-  benefits: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Benefit'
-  }],
-  leave: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Leave'
-  }],
-  attendance: [{
-    type: Schema.Types.ObjectId,
+    address: {
+      type: String
+    },
+    employmentStatus: {
+      type: String
+    },
+    tasks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Task"
+      }
+    ],
+    files: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "File"
+      }
+    ],
+    assets: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Asset"
+      }
+    ],
+    dependents: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Dependent"
+      }
+    ],
+    travels: {
+      type: Schema.Types.ObjectId,
+      ref: "Travel"
+    },
+    reportingTo: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+    skills: [],
+    benefits: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Benefit"
+      }
+    ],
+    leave: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Leave"
+      }
+    ],
+    attendance: [
+      {
+        type: Schema.Types.ObjectId,
 
-    ref: 'Attendance'
-  }],
-  messages: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Message'
-  }],
-  goals: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Goal'
-  }],
-  education: [],
-  previousJobExperience: [],
-  bioMessage: String,
-  jobDescription: String,
-  location: {
-    type: Schema.Types.ObjectId,
-    ref: 'Location'
+        ref: "Attendance"
+      }
+    ],
+    messages: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Message"
+      }
+    ],
+    goals: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Goal"
+      }
+    ],
+    education: [],
+    previousJobExperience: [],
+    bioMessage: String,
+    jobDescription: String,
+    location: {
+      type: Schema.Types.ObjectId,
+      ref: "Location"
     },
-  seatingLocation: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Location'
-    }],
-  hireSource: String,
-  expertise: [],
-  delegation: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Delegation'
-  }],
-  cases: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Case'
-  }],
-  organizations: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Organization'
-  }],
-  jobs: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Job'
-  }],
-  timeLogs: [{
-    type: Schema.Types.ObjectId,
-    ref: 'TimeLog'
-  }],
-  projects: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Project'
-  }],
-  services: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Service'
-  }],
-  shifts: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Shift'
-  }],
-  department: {
-    type: Schema.Types.ObjectId,
-    ref: 'Department'
+    seatingLocation: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Location"
+      }
+    ],
+    hireSource: String,
+    expertise: [],
+    delegation: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Delegation"
+      }
+    ],
+    cases: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Case"
+      }
+    ],
+    organizations: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Organization"
+      }
+    ],
+    jobs: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Job"
+      }
+    ],
+    timeLogs: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "TimeLog"
+      }
+    ],
+    projects: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Project"
+      }
+    ],
+    services: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Service"
+      }
+    ],
+    shifts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Shift"
+      }
+    ],
+    department: {
+      type: Schema.Types.ObjectId,
+      ref: "Department"
+    },
+    travels: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Travel"
+      }
+    ]
   },
-  travels: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Travel'
-  }]
+  { timestamps: true }
+);
+
+userSchema.pre("save", function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  bcrypt.hash(this.password, 8, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+
+    this.password = hash;
+    next();
+  });
 });
-export const User = mongoose.model('User', userSchema)
+
+userSchema.methods.checkPassword = function(password) {
+  const passwordHash = this.password;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, passwordHash, (err, same) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(same);
+    });
+  });
+};
+
+export const User = mongoose.model("User", userSchema);
