b376598b28b09b77921cd73b6ba84c2264b8fe51
Damola Adewunmi <damolasd@gmail.com>
2020-01-29 09:00:37 +0100

    added auth middleware file

---
+++ src/middlewares/auth.js
@@ -0,0 +1,71 @@
+const jwt = require("jsonwebtoken");
+const response = require("./response");
+const UserModel = require("../models/User");
+
+module.exports = {
+  async getUserFromToken(req, res) {
+    const { token } = req.headers;
+    if (!token) {
+      return response.error(res, 401, "Token is required");
+    }
+    try {
+      const decode = jwt.verify(token, process.env.jwtSecret);
+      return (req.decode = decode);
+    } catch (error) {
+      return response.error(res, 401, "Error token type");
+    }
+  },
+
+  async authUser(req, res, next) {
+    const user = await getUserFromToken(req, res);
+    if (user.username) {
+      try {
+        const { username } = req.params;
+        if (username !== user.username) {
+          return response.error(res, 401, "Wrong user");
+        }
+        return next();
+      } catch (error) {
+        return next({ message: "Error validating User" });
+      }
+    }
+  },
+
+  async isUserAdmin(req, res, next) {
+    const user = getUserFromToken(req, res);
+    if (user.username) {
+      try {
+        const { username } = req.params;
+        if (username !== user.username) {
+          return response.error(res, 401, "Wrong user");
+        }
+        const isAdmin = UserModel.findOne({ username }).exec();
+        if (isAdmin.role === "admin") {
+          return next();
+        }
+        return response.error({ message: "User is not admin " });
+      } catch (error) {
+        return next({ message: "Error validating user" });
+      }
+    }
+  },
+
+  async isUserManager(req, res, next) {
+    const user = getUserFromToken(req, res);
+    if (user.username) {
+      try {
+        const { username } = req.params;
+        if (username !== user.username) {
+          return response.error(res, 401, "Wrong user");
+        }
+        const isAdmin = UserModel.findOne({ username }).exec();
+        if (isAdmin.role === "manager") {
+          return departmen
+        }
+        return response.error({ message: "User is not admin " });
+      } catch (error) {
+        return next({ message: "Error validating user" });
+      }
+    }
+  },
+};
