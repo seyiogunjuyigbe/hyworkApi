a44db17fd439cafaf0e6431c607eed4e1a4a156e
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-02-06 17:43:20 +0100

    bugfix/passport, bycrypt errors fixed

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
 const passportLocalMongoose = require('passport-local-mongoose');
 const nodemailer = require('nodemailer');
 const cloudinary = require('cloudinary');
@@ -15,7 +15,7 @@ import {startDb} from './database/db'
 import {SECRET_KEY, SITE_URL} from "./config/constants"
 import {initRoutes} from './routes/routes'
 import {User} from './models/User';
-import { passportConfig } from './config/passport';
+// const passportConfig = require ('./config/passport');
 startDb();
 app.set('views', path.join(__dirname, 'views')) // Redirect to the views directory inside the src directory
 app.use(express.static(path.join(__dirname, '../public'))); // load local css and js files
@@ -31,15 +31,10 @@ app.use(require("express-session")({
 
 app.use(passport.initialize());
 app.use(passport.session());
-// passport.use("local", new LocalStrategy(User.authenticate()));
-passportConfig(passport);
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
     next();
 })
-
 
 initRoutes(app);
 
