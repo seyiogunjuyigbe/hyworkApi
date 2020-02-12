beed0df07dfb08eb2cbaa3c3fa64cfeac2a2b2ed
Damola Adewunmi <damola@touchcore.com.ng>
2020-02-12 14:51:00 +0100

    Added two routes for fetching all files and fetching files uploaded by user

--- src/controllers/fileManagementController.js
+++ src/controllers/fileManagementController.js
@@ -2,7 +2,7 @@ import { File } from "../models/File";
 const response = require('../middlewares/response');
 
 module.exports = {
-    async uploadFile(req, res) {
+    uploadFile(req, res) {
         const { title, description } = req.body;
         File.create({ title, description, fileLocationUrl: `uploads/files/${req.file.filename}`, uploadedBy: req.user._id }, (err, file) => {
             if (err) {
@@ -11,5 +11,34 @@ module.exports = {
             response.success(res, 200, "File uploaded successfully")
         })
 
+    },
+
+    async fetchAllFiles(req, res) {
+        try {
+            const files = await File.find({}).lean().exec()
+            if (!files) {
+                response.error(res, 400, "Could not fetch files");
+            }
+            response.success(res, 200, files)
+
+        } catch (error) {
+            response.error(res, 500, error.message)
+        }
+
+    }, 
+
+    async fetchFilesByUser(req, res) {
+        try {
+            const files = await File.find({ uploadedBy: req.user._id }).lean().exec()
+            if (!files) {
+                response.error(res, 400, `No files owned by user ${req.user.username}`);
+            }
+            response.success(res, 200, files)
+
+        } catch (error) {
+            response.error(res, 500, error.message)
+        }
+
     }
+
 }
--- src/routes/fileRoute.js
+++ src/routes/fileRoute.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 import { upload } from '../../utils/multer';
-import { uploadFile } from "../controllers/fileManagementController";
+import { uploadFile, fetchAllFiles, fetchFilesByUser } from "../controllers/fileManagementController";
 const authUser = require("../middlewares/middleware");
 
-
+router.get('/all', [authUser.authUser, authUser.isAdmin], fetchAllFiles);
+router.get('/myfiles', [authUser.authUser], fetchFilesByUser);
 router.post('/upload', [ authUser.authUser , upload.single('file')], uploadFile);
 
 
