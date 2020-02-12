7b630b7d5f45e51b4d67daf486049f81d89f3536
Damola Adewunmi <damola@touchcore.com.ng>
2020-02-12 16:12:57 +0100

    Update and GetbyId added

--- src/controllers/fileManagementController.js
+++ src/controllers/fileManagementController.js
@@ -1,19 +1,47 @@
 import { File } from "../models/File";
+import { crudControllers } from "../../utils/crud";
+
 const response = require('../middlewares/response');
 
+export default crudControllers(File);
+
+
 module.exports = {
-    uploadFile(req, res) {
+   async uploadFile (req, res) {
         const { title, description } = req.body;
         File.create({ title, description, fileLocationUrl: `uploads/files/${req.file.filename}`, uploadedBy: req.user._id }, (err, file) => {
             if (err) {
-                response.error(res, 500, err);
+                response.error(res, 400, err);
             }
             response.success(res, 200, "File uploaded successfully")
         })
 
     },
+ 
 
-    async fetchAllFiles(req, res) {
+    async fetchById (req, res) {
+        File.findById(req.params.id, (err, file) => {
+            if(err) {
+                response.error(res, 404, err)
+            }
+            response.success(res, 200, file);
+        })
+    },
+
+    async updateFileDetails (req, res) {
+        const { title, description } = req.body; 
+        const file = await File.findById(req.params.id);
+        file.title = title;
+        file.description = description;
+        file.save((err) => {
+            if(err) {
+                response.error(res, 404, err)
+            }
+            response.success(res, 200, "File Updated")
+        })
+    }, 
+    
+    async fetchAllFiles (req, res) {
         try {
             const files = await File.find({}).lean().exec()
             if (!files) {
@@ -27,7 +55,7 @@ module.exports = {
 
     }, 
 
-    async fetchFilesByUser(req, res) {
+    async fetchFilesByUser (req, res) {
         try {
             const files = await File.find({ uploadedBy: req.user._id }).lean().exec()
             if (!files) {
--- src/routes/fileRoute.js
+++ src/routes/fileRoute.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 import { upload } from '../../utils/multer';
-import { uploadFile, fetchAllFiles, fetchFilesByUser } from "../controllers/fileManagementController";
+import { uploadFile, fetchAllFiles, fetchFilesByUser, updateFileDetails, fetchById } from "../controllers/fileManagementController";
+// import controllers from "../controllers/fileManagementController";
 const authUser = require("../middlewares/middleware");
 
 router.get('/all', [authUser.authUser, authUser.isAdmin], fetchAllFiles);
 router.get('/myfiles', [authUser.authUser], fetchFilesByUser);
+router.get('/:id', [authUser.authUser], fetchById);
+router.post('/:id', [authUser.authUser], updateFileDetails);
 router.post('/upload', [ authUser.authUser , upload.single('file')], uploadFile);
 
 
