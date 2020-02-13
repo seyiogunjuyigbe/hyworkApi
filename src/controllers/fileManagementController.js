e7785f6a0213c961c70a1591e84ab59d737549eb
Damola Adewunmi <damola@touchcore.com.ng>
2020-02-13 11:38:33 +0100

    changed export format

--- src/controllers/fileManagementController.js
+++ src/controllers/fileManagementController.js
@@ -5,68 +5,66 @@ const response = require('../middlewares/response');
 
 export default crudControllers(File);
 
-
-module.exports = {
-   async uploadFile (req, res) {
-        const { title, description } = req.body;
-        File.create({ title, description, fileLocationUrl: `uploads/files/${req.file.filename}`, uploadedBy: req.user._id }, (err, file) => {
-            if (err) {
-                response.error(res, 400, err);
-            }
-            response.success(res, 200, "File uploaded successfully")
-        })
-
-    },
- 
-
-    async fetchById (req, res) {
-        File.findById(req.params.id, (err, file) => {
-            if(err) {
-                response.error(res, 404, err)
-            }
-            response.success(res, 200, file);
-        })
-    },
-
-    async updateFileDetails (req, res) {
-        const { title, description } = req.body; 
-        const file = await File.findById(req.params.id);
-        file.title = title;
-        file.description = description;
-        file.save((err) => {
-            if(err) {
-                response.error(res, 404, err)
-            }
-            response.success(res, 200, "File Updated")
-        })
-    }, 
-    
-    async fetchAllFiles (req, res) {
-        try {
-            const files = await File.find({}).lean().exec()
-            if (!files) {
-                response.error(res, 400, "Could not fetch files");
-            }
-            response.success(res, 200, files)
-
-        } catch (error) {
-            response.error(res, 500, error.message)
+export async function uploadFile(req, res) {
+    const { title, description } = req.body;
+    File.create({ title, description, fileLocationUrl: `uploads/files/${req.file.filename}`, uploadedBy: req.user._id }, (err, file) => {
+        if (err) {
+            response.error(res, 400, err);
         }
+        response.success(res, 200, "File uploaded successfully")
+    })
 
-    }, 
+}
 
-    async fetchFilesByUser (req, res) {
-        try {
-            const files = await File.find({ uploadedBy: req.user._id }).lean().exec()
-            if (!files) {
-                response.error(res, 400, `No files owned by user ${req.user.username}`);
-            }
-            response.success(res, 200, files)
 
-        } catch (error) {
-            response.error(res, 500, error.message)
+// export async function fetchById(req, res) {
+//     File.findById(req.params.id, (err, file) => {
+//         if (err) {
+//             response.error(res, 404, err)
+//         }
+//         response.success(res, 200, file);
+//     })
+// }
+
+export async function updateFileDetails(req, res) {
+    const { title, description } = req.body;
+    const file = await File.findById(req.params.id);
+    file.title = title;
+    file.description = description;
+    file.save((err) => {
+        if (err) {
+            response.error(res, 404, err)
         }
+        response.success(res, 200, "File Updated")
+    })
+}
 
+// export async function fetchAllFiles(req, res) {
+//     try {
+//         const files = await File.find({}).lean().exec()
+//         if (!files) {
+//             response.error(res, 400, "Could not fetch files");
+//         }
+//         response.success(res, 200, files)
+
+//     } catch (error) {
+//         response.error(res, 500, error.message)
+//     }
+
+// }
+
+export async function fetchFilesByUser(req, res) {
+    try {
+        const files = await File.find({ uploadedBy: req.user._id }).lean().exec()
+        if (!files) {
+            response.error(res, 400, `No files owned by user ${req.user.username}`);
+        }
+        response.success(res, 200, files)
+
+    } catch (error) {
+        response.error(res, 500, error.message)
     }
 
 }
+
+
--- src/routes/fileRoute.js
+++ src/routes/fileRoute.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 import { upload } from '../../utils/multer';
 import { uploadFile, fetchAllFiles, fetchFilesByUser, updateFileDetails, fetchById } from "../controllers/fileManagementController";
-// import controllers from "../controllers/fileManagementController";
+import controllers from "../controllers/fileManagementController";
 const authUser = require("../middlewares/middleware");
 
-router.get('/all', [authUser.authUser, authUser.isAdmin], fetchAllFiles);
+router.get('/all', [authUser.authUser, authUser.isAdmin], controllers.getMany);
 router.get('/myfiles', [authUser.authUser], fetchFilesByUser);
-router.get('/:id', [authUser.authUser], fetchById);
+router.get('/:id', [authUser.authUser], controllers.getOneById);
 router.post('/:id', [authUser.authUser], updateFileDetails);
 router.post('/upload', [ authUser.authUser , upload.single('file')], uploadFile);
+// router.delete('/:id', controllers.removeOne)
 
 
 module.exports = router;
