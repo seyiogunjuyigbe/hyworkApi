4a2f3be42bcf0939ead639963baabd462cf2c5ac
Damola Adewunmi <damola@touchcore.com.ng>
2020-02-19 11:20:49 +0100

    remove employee from department

--- src/controllers/department.js
+++ src/controllers/department.js
@@ -3,18 +3,18 @@ import { Department } from "../models/Department";
 import { User } from "../models/User";
 import { crudControllers } from "../../utils/crud";
 const response = require("../middlewares/response");
+const array = require('lodash/array');
 
 //Route: org/:urlname/department/create
 export const createDepartment = async (req, res) => {
     const { title, description } = req.body;
     console.log(title);
     try {
-        const org = Organization.findOne({ urlname: req.params.urlname });
+        const org = await Organization.findOne({ urlname: req.params.urlname });
         Department.create({ title, description, dateCreated: Date.now() }, (err, dept) => {
             if (err) {
                 response.error(res, 404, err)
             }
-            console.log(dept._id);
             org.department.push(dept._id);
             org.save();
             response.success(res, 200, `Added department ${dept.title} to organization ${org.name}`);
@@ -31,7 +31,7 @@ export const addManager = async (req, res) => {
 
     try {
         const org = await Organization.findOne({ urlname: req.params.urlname });
-        Department.findOne({ title }, (err, dept) => {
+        Department.findById(id, (err, dept) => {
             if (err) {
                 response.error(res, 404, err);
             }
@@ -39,13 +39,13 @@ export const addManager = async (req, res) => {
                 if (err) {
                     response.error(res, 404, err)
                 }
-                dept.manager = user._id;
-                dept.save((err) => {
-                    if (err) {
-                        response.error(err)
-                    }
+                if (org.department.includes(dept._id)) {
+                    dept.manager = user._id;
+                    dept.save()
                     response.success(res, 200, `Made User ${user.username} the manager of department ${dept.title}`)
-                })
+                } else {
+                    response.error(res, 404, `Department ${dept.title} not a department in organization ${org.name}`)
+                }
             })
         });
     } catch (error) {
@@ -58,46 +58,82 @@ export const addEmployee = async (req, res) => {
 
     try {
         const org = await Organization.findOne({ urlname: req.params.urlname });
-        Department.findById(id , (err, dept) => {
+        Department.findById(id, (err, dept) => {
             if (err) {
                 response.error(res, 404, err);
             }
             User.findOne({ username }, (err, user) => {
                 if (err) {
-                    response.error(res, 404, err)
+                    return response.error(res, 404, err)
                 }
-                if(dept.employees.includes(user._id)) {
-                    response.error(res, 500, `Employee ${user.username} is already a member of department ${dept.name}`)
+                if (dept.employees.includes(user._id)) {
+                    return response.error(res, 500, `${user.firstName} ${user.lastName} is already a member of department ${dept.title}`)
                 }
                 dept.employees.push(user._id);
                 dept.save();
-                response.success(res, 200, `Added ${user.username} to department ${dept.title}`);
+                return response.success(res, 200, `Added ${user.firstName} to department ${dept.title}`);
 
             })
         });
     } catch (error) {
-        response.error(res, 500, error.message)
+        return response.error(res, 500, error.message)
     }
 }
 
-// Route: /organization/:urlname/department/:title/add
-export const addDeptToOrg = async (req, res) => {
-    const { id, urlname } = req.params;
-    // console.log(title);
+
+//Remove employee from department
+export const removeEmployee = async (req, res) => {
+    const { id, username } = req.params;
+
     try {
-        const org = await Organization.findOne({ urlname });
+        const org = await Organization.findOne({ urlname: req.params.urlname });
         Department.findById(id, (err, dept) => {
             if (err) {
                 response.error(res, 404, err);
             }
             if (org.department.includes(dept._id)) {
-                response.error(res, 404, `Department ${dept.title} already exists in organization ${org.name}`)
+
+                User.findOne({ username }, (err, user) => {
+                    if (err) {
+                        return response.error(res, 404, err)
+                    }
+                    if (dept.employees.includes(user._id)) {
+                        // console.log(dept.employees);
+                        dept.employees = dept.employees.filter(value => {
+                            value === user._id
+                        });
+                        dept.save();
+                        return response.success(res, 200, `Removed ${user.firstName} from department ${dept.title}`);
+                    } else {
+                        return response.error(res, 404, `${user.firstName} is not a member of this department`)
+                    }
+                })
+
+            } else {
+                response.error(res, 404, `Department ${dept.title} not a department in organization ${org.name}`);
+            }
+        });
+    } catch (error) {
+        return response.error(res, 500, error.message)
+    }
+}
+// Route: /organization/:urlname/department/:title/add
+export const addDeptToOrg = async (req, res) => {
+    const { id, urlname } = req.params;
+    try {
+        const org = await Organization.findOne({ urlname });
+        Department.findById(id, (err, dept) => {
+            if (err) {
+                return response.error(res, 404, err);
+            }
+            if (org.department.includes(dept._id)) {
+                return response.error(res, 404, `Department ${dept.title} already exists in organization ${org.name}`)
             }
             org.department.push(dept._id);
             org.save();
-            response.success(res, 200, `Added department ${dept.title} to organization ${org.name}`);
+            return response.success(res, 200, `Added department ${dept.title} to organization ${org.name}`);
         });
     } catch (error) {
-        response.error(res, 500, error.message)
+        return response.error(res, 500, error.message)
     }
 }
--- src/routes/departmentRouter.js
+++ src/routes/departmentRouter.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-import { createDepartment, addDeptToOrg, addEmployee, addManager} from '../controllers/department';
+import { createDepartment, addDeptToOrg, addEmployee, addManager, removeEmployee} from '../controllers/department';
 const authUser = require("../middlewares/middleware");
 const { check } = require('express-validator');
 const validate = require("../middlewares/validate");
@@ -11,9 +11,10 @@ router.post('/:urlname/dept/create', [
     check("description").not().isEmpty().withMessage("Enter description of Department")
 ], [validate, authUser.orgExists], createDepartment);
 
-router.post('/:urlname/dept/:id/add', [authUser.orgExists], addDeptToOrg);
-router.post('/:urlname/dept/:id/addManager/:username', [authUser.isAdmin], addManager);
+router.post('/:urlname/dept/:id/add', [authUser.isAdmin, authUser.orgExists], addDeptToOrg);
+router.post('/:urlname/dept/:id/addManager/:username', [authUser.isAdmin, authUser.orgExists], addManager);
 
 router.post('/:urlname/dept/:id/addEmployee/:username', addEmployee);
+router.patch('/:urlname/dept/:id/removeEmployee/:username', removeEmployee);
 
 module.exports = router;
