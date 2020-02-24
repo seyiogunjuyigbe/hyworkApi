4e0a0d8200f5cdd793e39f4b9f0ee451844ee4a2
Damola Adewunmi <damola@touchcore.com.ng>
2020-02-24 09:50:12 +0100

    Add user phone number endpoints done

---
+++ src/controllers/user.js
@@ -0,0 +1,30 @@
+const response = require('../middlewares/response');
+import { User } from '../models/User';
+
+export const addPhoneNumber = async (req, res) => {
+    const { phone } = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(req.user._id,{
+            phoneNumber: phone
+        });
+        if (user) {
+            response.success(res, 200, `User's Phone Number Updated`)
+        }
+    }catch(error) {
+        response.error(res, 500, error.message)
+    }
+}
+
+export const addPhoneNumber2 = async (req, res) => {
+    const { phone } = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(req.user._id,{
+            phoneNumber2: phone
+        });
+        if (user) {
+            response.success(res, 200, `User's Phone Number Updated`)
+        }
+    }catch(error) {
+        response.error(res, 500, error.message)
+    }
+}
