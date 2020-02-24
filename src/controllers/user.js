8fddfd3a280b1aec11bde511ba93b526b0767513
Damola Adewunmi <damola@touchcore.com.ng>
2020-02-24 10:16:58 +0100

    add BioData

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -28,3 +28,16 @@ export const addPhoneNumber2 = async (req, res) => {
         response.error(res, 500, error.message)
     }
 }
+
+export const addBioData = async (req, res) => {
+    const { dob, maritalStatus, bioMessage } = req.body;
+
+    try {
+        const user = await User.findByIdAndUpdate(req.user._id, { maritalStatus, dob, bioMessage });
+        if(user) {
+            response.success(res, 200, "Updated Bio Details")
+        }
+    }catch(error) {
+        response.error(res, 500, error.message);
+    }
+}
