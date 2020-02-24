a271406114a955ae8871041caccadf42f2a07215
Damola Adewunmi <damola@touchcore.com.ng>
2020-02-24 10:33:47 +0100

    Refactored add phoneNumber

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,40 +4,37 @@ import { User } from '../models/User';
 export const addPhoneNumber = async (req, res) => {
     const { phone } = req.body;
     try {
-        const user = await User.findByIdAndUpdate(req.user._id,{
-            phoneNumber: phone
-        });
+        const user = await User.findById(req.user._id);
         if (user) {
-            response.success(res, 200, `User's Phone Number Updated`)
+            if (user.phoneNumber === "") {
+                user.phoneNumber = phone;
+                user.save().then(
+                    response.success(res, 200, `User's Phone Number Added`)
+                )
+            }else if(user.phoneNumber2 === ""){
+                user.phoneNumber2 = phone;
+                user.save().then(
+                    response.success(res, 200, `User's Second Phone Number Added`)
+                )
+            }
+            response.error(res, 404, `Phone Numbers already exists`);
         }
-    }catch(error) {
+    } catch (error) {
         response.error(res, 500, error.message)
     }
 }
 
-export const addPhoneNumber2 = async (req, res) => {
-    const { phone } = req.body;
-    try {
-        const user = await User.findByIdAndUpdate(req.user._id,{
-            phoneNumber2: phone
-        });
-        if (user) {
-            response.success(res, 200, `User's Phone Number Updated`)
-        }
-    }catch(error) {
-        response.error(res, 500, error.message)
-    }
-}
 
 export const addBioData = async (req, res) => {
     const { dob, maritalStatus, bioMessage } = req.body;
 
     try {
         const user = await User.findByIdAndUpdate(req.user._id, { maritalStatus, dob, bioMessage });
-        if(user) {
+        if (user) {
             response.success(res, 200, "Updated Bio Details")
         }
-    }catch(error) {
+    } catch (error) {
         response.error(res, 500, error.message);
     }
 }
+
