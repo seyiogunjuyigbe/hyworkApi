c181c98da11ae295c209b7f60314a2ba0b117fd8
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-03-12 22:45:25 +0100

    assign job

--- src/controllers/job.js
+++ src/controllers/job.js
@@ -19,7 +19,7 @@ export const createJob = (req,res)=>{
             org.employees.forEach(employee=>list.push(employee.username)); 
             if(typeof(req.body.assignees) !== 'undefined'){
                 if(!list.includes(...req.body.assignees)){
-                return response.error(res,422, 'You have included a respondent who is not an employee of this organization');
+                return response.error(res,422, 'You have included an assignee who is not an employee of this organization');
             }
             else{
                 req.body.assignees.forEach((assignee)=>{
@@ -83,5 +83,107 @@ Job.findById(req.params.job_id, (err,thisJob)=>{
 // Method: POST
 // Access: logged in employee who is the job creator
 export const assignThisJob = (req,res)=>{
-    
+    Organization.findOne({urlname}).populate("employees", "email username -_id").exec((err,org)=>{
+        if(err) return response.error(res,500,err.message)
+        else if(!org) return response.error(res,404, 'Org not found')
+        else{
+            var list  = [];
+            var emailList = []
+            org.employees.forEach(employee=>list.push(employee.username)); 
+            if(typeof(req.body.assignees) !== 'undefined'){
+                if(!list.includes(...req.body.assignees)){
+                return response.error(res,422, 'You have included an assignee who is not an employee of this organization');
+            }
+            else{
+                req.body.assignees.forEach((assignee)=>{
+                    var i = list.indexOf(assignee);
+                    emailList.push(org.employees[i].email);
+                })
+            }}
+            Job.findById(req.params._id, (err,thisJob)=>{
+                if(err) return response.error(res,500,err.message);
+                else if(!thisJob) return response.error(res,404,'Requested job not found');
+                else if(thisJob.createdBy !==req.user.username) return response.error(res,403,'You are nit authoeized to assign this job');
+                else {
+                    thisJob.assignees.push(...req.body.assignees);
+                    thisJob.save();
+                    var mailoptions = {
+                        from: MAIL_SENDER,
+                            to: emailList,
+                            subject: `A job has been assigned to you`,
+                            html: 
+                                ` 
+                                <p style="font-family:candara;font-size:1.2em">Hi, you have been assigned to a new job.</p>
+                                <ul>
+                                <li style="font-family:candara;font-size:1.2em">Subject: ${thisJob.title}</li>
+                                <li style="font-family:candara;font-size:1.2em">Duration: ${thisJob.hours} hours</li>
+                                <li style="font-family:candara;font-size:1.2em">Paid job: ${thisJob.isBillable}</li>
+                                <li style="font-family:candara;font-size:1.2em">Details: ${thisJob.description}</li>
+                                </ul>
+                                <a href="http://${req.headers.host}/org/${org.urlname}/job:/" style="font-family:candara;font-size:1.2em">Click to view job</a>
+                                    `,                  
+                                  }
+                                  if(emailList.length >= 1){
+                                      sendMailToTheseUsers(req,res,mailoptions)
+                                  }
+                                  return response.success(res,200,thisJob)
+                }
+            })
+        }
+    })
+}
+
+
+// Update a job
+// @Params: urlname, job_id
+// Method: POST
+// Access: logged in employee who is the job creator
+export const updateThisJob = (req,res)=>{
+    Organization.findOne({urlname}).populate("employees", "email username -_id").exec((err,org)=>{
+        if(err) return response.error(res,500,err.message)
+        else if(!org) return response.error(res,404, 'Org not found')
+        else{
+            var list  = [];
+            var emailList = []
+            org.employees.forEach(employee=>list.push(employee.username)); 
+            if(typeof(req.body.assignees) !== 'undefined'){
+                if(!list.includes(...req.body.assignees)){
+                return response.error(res,422, 'You have included an assignee who is not an employee of this organization');
+            }
+            else{
+                req.body.assignees.forEach((assignee)=>{
+                    var i = list.indexOf(assignee);
+                    emailList.push(org.employees[i].email);
+                })
+            }}
+            Job.findById(req.params._id, (err,thisJob)=>{
+                if(err) return response.error(res,500,err.message);
+                else if(!thisJob) return response.error(res,404,'Requested job not found')
+                else {
+                    thisJob.assignees.push(...req.body.assignees);
+                    thisJob.save();
+                    var mailoptions = {
+                        from: MAIL_SENDER,
+                            to: emailList,
+                            subject: `A job has been assigned to you`,
+                            html: 
+                                ` 
+                                <p style="font-family:candara;font-size:1.2em">Hi, you have been assigned to a new job.</p>
+                                <ul>
+                                <li style="font-family:candara;font-size:1.2em">Subject: ${thisJob.title}</li>
+                                <li style="font-family:candara;font-size:1.2em">Duration: ${thisJob.hours} hours</li>
+                                <li style="font-family:candara;font-size:1.2em">Paid job: ${thisJob.isBillable}</li>
+                                <li style="font-family:candara;font-size:1.2em">Details: ${thisJob.description}</li>
+                                </ul>
+                                <a href="http://${req.headers.host}/org/${org.urlname}/job:/" style="font-family:candara;font-size:1.2em">Click to view job</a>
+                                    `,                  
+                                  }
+                                  if(emailList.length >= 1){
+                                      sendMailToTheseUsers(req,res,mailoptions)
+                                  }
+                                  return response.success(res,200,thisJob)
+                }
+            })
+        }
+    })
 }
--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -1,5 +1,5 @@
 const router = require ('express').Router();
-import { createJob,fetchThisJob } from "../controllers/job";
+import { createJob,fetchThisJob, assignThisJob } from "../controllers/job";
 import { LoggedUserisEmployee, LoggedUserisAdmin } from "../middlewares/organization";
 import { check } from "express-validator";
 import { authUser } from "../middlewares/middleware";
@@ -8,6 +8,9 @@ router.post('/:urlname/job/new', authUser, LoggedUserisEmployee,LoggedUserisAdmi
     check('title').not().isEmpty().withMessage('Please give this job a title'),
     check('hours').isNumeric().not().isEmpty().withMessage('Please specify number of hours for this job')
 ], validate, createJob)
+router.post('/:urlname/job/:job_id/assign', authUser, LoggedUserisEmployee,LoggedUserisAdmin, [
+    check('assignees').isArray().not().isEmpty().withMessage('Please assign this job to an employee')
+], validate, assignThisJob)
 
 router.get('/:urlname/job/:job_id/view', authUser,LoggedUserisEmployee,fetchThisJob)
 module.exports = router;
