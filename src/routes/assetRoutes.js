e416f80f658a16f0f6310dc1ab06a9e9ddfeab74
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2020-04-20 07:15:47 +0100

    fetch assets

--- src/routes/assetRoutes.js
+++ src/routes/assetRoutes.js
@@ -3,17 +3,19 @@ const validate = require('../middlewares/validate');
 const { check } = require('express-validator');
 import {authUser} from '../middlewares/middleware';
 import {LoggedUserisEmployee, LoggedUserisAdmin} from '../middlewares/organization';
-import {createAsset, modifyAsset, deleteAsset} from '../controllers/asset'
+import {createAsset, modifyAsset, deleteAsset, fetchAssets} from '../controllers/asset'
 
 router.post('/:urlname/asset/new', [
     check('title').isString().not().isEmpty().withMessage('This asset needs a title'),
     check('description').isString().withMessage('Pleade enter a valid string as description'),
     check('acquiredBy').isString().not().isEmpty().withMessage('Please specify the recipient of this asset'),
-], validate, authUser, createAsset);
+], validate, authUser, LoggedUserisAdmin, createAsset);
 router.post('/:urlname/asset/:asset_id/update', [
     check('title').isString().not().isEmpty().withMessage('This asset needs a title'),
     check('description').isString().withMessage('Please enter a valid string as description'),
     check('acquiredBy').isString().not().isEmpty().withMessage('Please specify the recipient of this asset'),
 ], validate, authUser, modifyAsset)
-router.get("/:urlname/asset/:asset_id/delete", authUser, deleteAsset)
+router.get("/:urlname/asset/:asset_id/delete", authUser, LoggedUserisAdmin, deleteAsset)
+router.get("/:urlname/asset/fetch", authUser, LoggedUserisAdmin, fetchAssets)
+
 module.exports = router;
